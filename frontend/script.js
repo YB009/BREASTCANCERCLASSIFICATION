6cdca99ef49776488ac4bf32a26d451e1b74eeb4
YB009 <owohdaniel09@gmail.com>
2025-05-05 06:24:25 +0100

    NEW COMMIT

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/predict'; // Change to your Render URL when deploying
+const API_URL = 'https://breastcancerclassification.onrender.com/'; // Change to your Render URL when deploying
 
 const featureNames = [
     "radius_mean", "texture_mean", "perimeter_mean", "area_mean",
